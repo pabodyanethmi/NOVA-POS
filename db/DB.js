086c92e87cdedb117f0a8147000fa08b8874070e
Pabodya Kandambi <youremail@example.com>
2026-04-25 17:12:08 +0530

    Developing POS

---
+++ db/DB.js
@@ -0,0 +1,32 @@
+export let Customers = [
+  {
+    custId: "#C001",
+    custName: "Pabodya",
+    custAddress: "Galle",
+    custSalary: "95000",
+  },
+
+  {
+    custId: "#C002",
+    custName: "Sanjana",
+    custAddress: "Kandy",
+    custSalary: "22000",
+  },
+];
+
+export let Items = [
+  {
+    itemId: "#I001",
+    itemName: "Book",
+    itemQty: 10,
+    itemPrice: 250,
+  },
+  {
+    itemId: "#I002",
+    itemName: "Pen",
+    itemQty: 50,
+    itemPrice: 50,
+  },
+];
+
+export let Orders = [];
